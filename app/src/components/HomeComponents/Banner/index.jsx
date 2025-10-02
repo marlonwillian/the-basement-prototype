b8192e2c59f03aa2d33a6a32a7dfa23cf98185ea
Marlon Willian Silva Barros <mwsb2004@gmail.com>
2025-10-01 22:17:39 -0300

    Navigator, Banner e início do conceito

---
+++ app/src/components/HomeComponents/Banner/index.jsx
@@ -0,0 +1,11 @@
+import styles from "./Banner.module.css";
+
+function Banner({ children }) {
+  return (
+    <div>
+      { children }
+    </div>
+  );
+}
+
+export default Banner;
---
+++ app/src/components/Navigator/index.jsx
@@ -0,0 +1,11 @@
+import styles from "./Navigator.module.css";
+
+function Navigator() {
+  return (
+    <header>
+      
+    </header>
+  );
+}
+
+export default Navigator;
